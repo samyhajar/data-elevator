a878fd10dd86d90f9e2889c717d20b8f4cd1d150
samyhajar <samy.hajar@gmail.com>
2021-12-09 22:24:41 +0100

    adjust zoom at landing page

--- data-elevator/src/App.js
+++ data-elevator/src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div>
       <h1>Data Elevator </h1>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[51.505, -0.09]} zoom={3} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
